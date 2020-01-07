94b1e78a265322780c84bce573bb40deab938725
MiroTW <43741555+MiroTW@users.noreply.github.com>
2020-01-07 10:32:57 +0800

    updated

--- signin.py
+++ signin.py
@@ -23,7 +23,7 @@ class singin(unittest.TestCase):
         self.driver.find_element_by_id('submitBtn').click()
         time.sleep(5)
         self.driver.find_element_by_xpath('//*[@id="global_bk"]/ul/li[2]/ul/li[5]/a').is_displayed()
-        self.driver.get_screenshot_as_file('D://test_104_login.png')
+        self.driver.get_screenshot_as_file('./test_104_login.png')
 
         if 'snoopy099' == password:
             print("password is same")
